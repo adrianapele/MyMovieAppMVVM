45d2fddffb765c2efb06e7c70274c3789f63aaf5
apele <pele_adriana@yahoo.com>
2020-05-30 11:10:39 +0300

    add nav drawer

--- app/src/main/java/com/example/mymovieapp/data/database/MovieDatabase.java
+++ app/src/main/java/com/example/mymovieapp/data/database/MovieDatabase.java
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.mymovieapp.data.model.Movie;
 
-@Database(entities = {Movie.class}, version = 1)
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
 public abstract class MovieDatabase extends RoomDatabase
 {
     private static MovieDatabase instance;
--- app/src/main/java/com/example/mymovieapp/view/MainActivity.java
+++ app/src/main/java/com/example/mymovieapp/view/MainActivity.java
@@ -1,16 +1,101 @@
 package com.example.mymovieapp.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.mymovieapp.R;
+import com.example.mymovieapp.view.favorites.FavoritesFragment;
+import com.example.mymovieapp.view.home.HomeFragment;
+import com.example.mymovieapp.view.search.SearchFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+    private DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null)
+        {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.nav_home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new HomeFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_favorite:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new FavoritesFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_search:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new SearchFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_share:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:pele_adriana@yahoo.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "My Movie List");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi there! Checkout my favorites movie list");
+                startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
