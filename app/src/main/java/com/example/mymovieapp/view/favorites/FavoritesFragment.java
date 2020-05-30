c905faeb7438f799a98abca90192820777495de5
apele <pele_adriana@yahoo.com>
2020-05-30 11:08:34 +0300

    add fragments

---
+++ app/src/main/java/com/example/mymovieapp/view/favorites/FavoritesFragment.java
@@ -0,0 +1,20 @@
+package com.example.mymovieapp.view.favorites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mymovieapp.R;
+
+public class FavoritesFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mymovieapp/view/search/SearchFragment.java
@@ -0,0 +1,20 @@
+package com.example.mymovieapp.view.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mymovieapp.R;
+
+public class SearchFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
